dc76601cd0c08f56b08b7b4c99767a04bad521f5
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2015-12-14 15:02:34 +0200

    OTP test

---
+++ otp-tests/locustfile.py
@@ -0,0 +1,21 @@
+from random import choice
+from locust import HttpLocust, TaskSet, task
+import simplejson
+
+url_params_json = simplejson.load(open("url_params.json"))
+set_of_url_params = url_params_json["url_params"]
+
+class OTPBehavior(TaskSet):
+    min_wait = 100
+    max_wait = 500
+
+    @task(2)
+    def index(self):
+        # CSS and SVG sprites are embedded in the HTML
+        url_params = choice(set_of_url_params)
+        self.client.get(url_params, name="otp")
+
+
+class RouteSearchingUser(HttpLocust):
+    task_set = OTPBehavior
+    host = 'http://dev.digitransit.fi/otp/otp/routers/default/'
